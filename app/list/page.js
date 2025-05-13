206ac4c42787243912b8af5072a2baa9a04e1315
밍 <sucresucces@icloud.com>
2025-05-13 14:31:24 +0900

    map 반복문
    여러 자료를 한 곳에 보관하려면 array 자료형
    
    array에서 데이터 하나만 꺼내려면
    a[0]
    
    array의 데이터 수정은
    a[0] = 100
    
    html 사이에 for, if 등 사용불가능
    => map() 쓰면 html 반복생성 가능
    
    map()기능
    1. 함수안의 코드 반복실행해줌
    2. array안의 자료 출력해줌
    3. return에 적은걸 array로 담아줌
    
    let 어레이 = [2, 3, 4]; // array 자료형 [데이터1, 데이터2...]
      let b = 어레이.map((a, i) => {
        // console.log(i);
        // i : 반복될 때 마다 0부터 1씩 커지는 정수
        return 10;
    
    map 반복문 사용시
    key={ 유니크한 값 } 넣으면 좋음

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,21 @@
 export default function List() {
   //3. export default 붙이기 1. function 작명(){}
+
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     // 2. return(HTML~~~)
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {상품.map((a, i) => {
+        return (
+          <div className="food" key={i}>
+            <h4>{a} $40</h4>
+            {/* <h4>{상품[i]} $40</h4> */}
+            {/* {상품[i]} == {a} */}
+          </div>
+        );
+      })}
     </div>
   );
 }

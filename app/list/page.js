1f506a067ca9b50a2e981b6d98f859f073e9cbe2
밍 <sucresucces@icloud.com>
2025-05-13 15:00:30 +0900

    image 넣기
    일반 이미지 넣는 법
    <img src="경로"/>
    
    alt = 이미지 설명
    className = 클래스네임 넣어서 스타일 지정
    
    + lazy loading
    + 사이즈 최적화
    + layout shift 방지
    => 최적화된 이미지로 넣을 수 있음
    
    최적화된 이미지 넣으려면
    1. import Image from 'next/image'
    2. <Image /> 사용
    3. 이미지를 import해서 경로 넣어야함
    
    import :  다른파일 가져오는 자바스크립트 문법
    
    외부이미지는 width, height 속성 필요함
    그리고 next.config.js 셋팅도 필요함
      // 외부 이미지 사용시
    /** @type {import('next').NextConfig} */
    const nextConfig = {
      experimental: {
        appDir: true,
      },
         images: {
           remotePatterns: [
             {
               protocol: "https",
               hostname: "s3.amazonaws.com", // 도메인 등록(이 사이트에 있는 이미지)
               port: "",
               pathname: "/my-bucket/**", // 경로 등록
             },
           ],
         },
    };
    
    export default nextConfig;
    
    최적화는 보통 다 만든 후에 하면 좋음
    
    <img src={"/food" + i + ".jpg"} className="food-img" />

--- app/list/page.js
+++ app/list/page.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+// import 사진 from "/public/food1.jpg"; // 아니면 @/public/food1.jpg
+
 export default function List() {
   //3. export default 붙이기 1. function 작명(){}
 
@@ -10,6 +13,12 @@ export default function List() {
       {상품.map((a, i) => {
         return (
           <div className="food" key={i}>
+            <img src={`/food${i}.jpg`} className="food-img" />
+
+            {/* <Image src={사진} className="food-img" /> */}
+
+            {/* 외부이미지 사용시 */}
+            {/* <Image src="https://어쩌고~" width={500} height={400} className="food-img" /> */}
             <h4>{a} $40</h4>
             {/* <h4>{상품[i]} $40</h4> */}
             {/* {상품[i]} == {a} */}
